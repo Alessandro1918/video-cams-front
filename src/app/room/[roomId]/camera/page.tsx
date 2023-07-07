a6e260d584e5a7742fd9ac5adb02510ad76b5a28
Alessandro1918 <alessandro1918@hotmail.com>
2023-07-07 18:15:22 -0300

    feat: basic page routing

---
+++ src/app/room/[roomId]/camera/page.tsx
@@ -0,0 +1,8 @@
+export default function Camera() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <h1>Camera</h1>
+    </main>
+  )
+}
---
+++ src/app/room/[roomId]/player/page.tsx
@@ -0,0 +1,8 @@
+export default function Player() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <h1>Player</h1>
+    </main>
+  )
+}
